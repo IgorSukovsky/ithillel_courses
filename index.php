2b13a7005016d1e9b2d61d14cd3655e52deb8e3d
Igor Sukovsky <igorsukovsky@MacBook-Air-Igor.local>
2024-02-27 16:32:54 +0200

    added Bootstrap CSS, folders, git repository, page screenshot

--- index.php
+++ index.php
@@ -1,2 +1,20 @@
-<?php
-require_once 'src/homework-1/index.php';
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homework Pages</title>
+    <!-- Подключение Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+<div class="container">
+    <h1>Choose a Homework:</h1>
+    <ul class="list-group">
+        <li class="list-group-item"><a href="src/homework-1/index.php">Homework 1</a></li>
+        <li class="list-group-item"><a href="src/homework-2/index.php">Homework 2</a></li>
+        <li class="list-group-item"><a href="src/homework-3/index.php">Homework 3</a></li>
+    </ul>
+</div>
+</body>
+</html>
