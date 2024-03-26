124e19d6e26536d10c8ef17cf988e485567f03c5
Igor Sukovsky <igorsukovsky@MacBook-Air-Igor.local>
2024-03-26 13:38:11 +0200

    added composer

--- src/homework-7/ParentClass.php
+++ src/homework-7/ParentClass.php
@@ -1,6 +1,29 @@
 <?php
 
-class ParentClass
+abstract class ParentClass
 {
+    protected float $number;
+    protected int $power;
 
+    public function getNumber(): float
+    {
+        return $this->number;
+    }
+
+    public function setNumber(float $number): void
+    {
+        $this->number = $number;
+    }
+
+    public function getPower(): int
+    {
+        return $this->power;
+    }
+
+    public function setPower(int $power): void
+    {
+        $this->power = $power;
+    }
+
+    abstract public function powerUp(): float;
 }
